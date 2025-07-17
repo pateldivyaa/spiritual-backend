f53fdd2e89db14c433368e9ccc12a94ba7456b59
pateldivyaa <pateldivyad3233@gmail.com>
2025-07-17 13:55:38 +0530

    fix: added dotenv package

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+
 const dotenv = require('dotenv')
 dotenv.config()
+require('dotenv').config()
+
 
 const app = express()
 const port = process.env.PORT || 3000
