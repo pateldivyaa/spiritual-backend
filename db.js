51bf5c32f3ecb958d55b052b902a055d7db4c086
pateldivyaa <pateldivyad3233@gmail.com>
2025-07-18 13:37:13 +0530

    fix: connected MongoDB Atlas properly

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('✅ MongoDB connected'))
+  .then(() => console.log('✅ MongoDB Atlas connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
 module.exports = mongoose;
